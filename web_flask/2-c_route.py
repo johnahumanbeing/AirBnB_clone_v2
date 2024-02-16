b6d7cea9d675460dcf41691942a29d51825380dd
johnahumanbeing <mseewak@gmail.com>
2024-02-16 21:10:44 +0300

    script that starts a Flask web application (displays c is ...)

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,10 +7,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def index():
     """Displays Hello HBNB!"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
@@ -19,12 +21,15 @@ def hbnb():
     """
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def display_c(text):
     """
     Displays 'C' then value of text variable
     """
+
     return 'C ' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
